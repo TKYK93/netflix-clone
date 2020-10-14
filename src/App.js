5c907c56f455736efbd94741b560656f3fa7f473
TKYK93 <laylalayla2905@gmail.com>
2020-10-14 15:38:59 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import requests from './request';
 import Row from './Row';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Nav />
       <Banner/>
 
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from "./request";
+import './Banner.css';
 
 const Banner = () => {
 const [movie, setMovie] = useState([]);
@@ -21,6 +22,9 @@ useEffect(() => {
     
     },[]);
 
+    const truncate = (str,n) => {
+        return str?.length > n ? str.substr(0, n-1) + " ..." : str;
+    };
 
     return (
         <header className="banner"
@@ -35,16 +39,22 @@ useEffect(() => {
 
             <div className="banner_contents">
             {/* title */}
-            <h1>
+            <h1 className="banner_title">
                 {/* もしーでなければ */}
                 {movie?.title || movie?.name || movie?.original_name}
             </h1>
 
-            <div></div>
-            {/* div two buttons */}
-            {/* discription */}
+            <div className= "banner_buttons">
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My List</button>
             </div>
-         
+
+           <h1 className="banner_description">
+               {truncate(movie?.overview, 150)}
+           </h1>
+            </div>
+
+           <div className="banner_fadeBottom"/>
         </header>
     )
 }
